3edaa3de892978877f251502791bbb3da3de7754
sreenu-341 <sreenivasulu.p20@gmail.com>
2020-08-28 18:10:14 +0530

    new files

---
+++ task1_atm.py
@@ -0,0 +1,34 @@
+pin=int(input('enter your pin number: \t'))
+if pin==112233:
+    print('pin is satisfied')
+    account=input(' seleect your account type : \t')
+    
+    if account=='saving':
+        print('account type is matched')
+        amount=int(input('enter the amount to withdraw : \t'))
+        balance=100000
+        if amount<balance:
+            print(amount, 'is debited from your account')
+        else:
+            print('insufficient funds in your account')
+    else:
+        print('account type is not matched')
+
+else:
+    print('pin is incorrect')
+
+
+
+'''
+account=str(input(' seleect your account type : \t'))
+if acoount=='saving':
+    print('account number is matched')
+    amount=int(input('enter the amount to withdraw : \t'))
+    balance=100000
+    if amount<balance:
+        print(amount, 'is debited from your account')
+    else:
+        print('insufficient funds in your account')
+else:
+    print('account number is not matched')
+'''
---
+++ task1_grades.py
@@ -0,0 +1,13 @@
+marks=float(input(' enter your percentage : \t'))
+if marks>=70:
+    print(' you got ditinction')
+elif marks>=60 and marks<70:
+    print('you got first class')
+elif marks>=50 and marks<60:
+    print('you got second cless')
+elif marks>=40 and marks<50:
+    print('you got third cless')
+elif marks>=35 and marks<40:
+    print('you are  just passed')
+else:
+    print('you are failed')
